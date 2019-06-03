28e10587a5d3c9326d07bdb054e65fde4fb49af4
Kacper Wojakowski <kacp.woja@gmail.com>
2019-06-03 22:55:58 +0200

    options

--- source/main.c
+++ source/main.c
@@ -16,22 +16,66 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	if(argc > 3)
-	{
-		printf("Too many input arguments. Excess arguments will be ignored.\n");
-	}
-
-	void (*copy_fun)(int, int) = &copy_rw;
+	void (*copy_fun)(int, int);
+	char* in_file;
+	char* out_file;
 	
+	char m_flag = 0;
+	char c;
+
 	while((c = getopt(argc, argv, "mh")) != -1)
 	{
 		switch(c)
 		{
 			case 'h':
-			h_flag = 1;
-			break
+				help();
+				return 0;
+			case 'm':
+				m_flag = 1;
+				break;
+			case '?':
+				fprintf(stderr, "Unknown option '-%c'\n", optopt);
+				return 1;
+			default:
+				fprintf(stderr, "getopt error\n");
+				return 2;
 		}
-	printf("EOPSY\n");
+	}
+
+	if(m_flag)
+	{
+		if(argc > 4)
+		{
+			printf("Too many input arguments. Excess arguments will be ignored.\n");
+		}
+		else if(argc < 4)
+		{
+			fprintf(stderr, "Too few input arguments.\nUse copy [-h] to see correct usage.\n");
+			return 3;
+		}
+		in_file = argv[2];
+		out_file = argv[3];
+		copy_fun = &copy_mm;
+	}
+	else
+	{
+		if(argc > 3)
+		{
+			printf("Too many input arguments. Excess arguments will be ignored.\n");
+		}
+		else if(argc < 3)
+		{
+			fprintf(stderr, "Too few input arguments.\nUse copy [-h] to see correct usage.\n");
+			return 3;
+		}
+		in_file = argv[1];
+		out_file = argv[2];
+		copy_fun = &copy_rw;
+	}
+
+	printf("In: %s out: %s\n", in_file, out_file);
+	(*copy_fun)(0,0);
+		
 	return 0;
 }
 
