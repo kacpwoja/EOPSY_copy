94e71ae9c21cd5601bc2d4ab9d3b1f0f08f712ee
Kacper Piotr Wojakowski <kacp.woja@gmail.com>
2019-06-05 18:48:49 +0200

    comments and help

--- source/main.c
+++ source/main.c
@@ -7,15 +7,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Size of a chunk used in copying with read()/write(), in bytes
 // 0 means filesize
-#define RW_CHUNK_SIZE 2
+#define RW_CHUNK_SIZE 0
 
+// Printing help info
 void help();
+// Copying functions
 void copy_rw(int fd_from, int fd_to);
 void copy_mm(int fd_from, int fd_to);
 
 int main(int argc, char** argv)
 {
+	// No arguments given
 	if(argc == 1)
 	{
 		help();
@@ -28,6 +32,7 @@ int main(int argc, char** argv)
 	int in_fd;
 	int out_fd;
 	
+	// Getting options
 	char m_flag = 0;
 	char c;
 
@@ -50,6 +55,7 @@ int main(int argc, char** argv)
 		}
 	}
 
+	// Memory Map mode
 	if(m_flag)
 	{
 		if(argc > 4)
@@ -61,10 +67,12 @@ int main(int argc, char** argv)
 			fprintf(stderr, "Too few input arguments.\nUse copy [-h] to see correct usage.\n");
 			exit(1);
 		}
+		// Choose filenames and function
 		in_file = argv[2];
 		out_file = argv[3];
 		copy_fun = &copy_mm;
 	}
+	// Read-Write mode
 	else
 	{
 		if(argc > 3)
@@ -76,17 +84,20 @@ int main(int argc, char** argv)
 			fprintf(stderr, "Too few input arguments.\nUse copy [-h] to see correct usage.\n");
 			exit(1);
 		}
+		// Choose filenames and function
 		in_file = argv[1];
 		out_file = argv[2];
 		copy_fun = &copy_rw;
 	}
 
+	// Opening the files
 	in_fd = open(in_file, O_RDONLY);
 	if(in_fd == -1)
 	{
 		perror("Input file open error\n");
 		exit(1);
 	}
+	// Getting input file access rights to copy them
 	struct stat buf;
 	if(fstat(in_fd, &buf) == -1)
 	{
@@ -94,6 +105,7 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
+	// Opening the output file
 	out_fd = open(out_file, O_RDWR | O_CREAT, buf.st_mode);
 	if(in_fd == -1)
 	{
@@ -101,8 +113,10 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 
+	// Copying
 	(*copy_fun)(in_fd, out_fd);
 
+	// Closing file descriptors
 	close(in_fd);
 	close(out_fd);
 		
@@ -111,13 +125,22 @@ int main(int argc, char** argv)
 
 void help()
 {
-	printf("Help Info\n");
+	printf("Program copy.\n");
+	printf("Copies a file to another file.\n");
+	printf("Correct usage:\n\n");
+	printf("\tcopy [-m] <file_name> <new_file_name>\n");
+	printf("\tcopy [-h]\n\n");
+	printf("Option -m enables memory mapping, when it is not specified the program uses reading and writing.\n");
+	printf("<file_name> is the name of the file to be copied\n");
+	printf("<new_file_name> is the target file.\n");
+	printf("Running with -h or no arguments shows this help.\n");
 }
 
 void copy_rw(int fd_from, int fd_to)
 {
 	size_t chunk;
 
+	// If chunk size = 0, load the size of input file and set it as chunk size
 	if(!RW_CHUNK_SIZE)
 	{
 		struct stat buf;
@@ -134,9 +157,11 @@ void copy_rw(int fd_from, int fd_to)
 		chunk = RW_CHUNK_SIZE;
 	}
 
-	char buffer[chunk];
+	// Buffer to move data through
+	char* buffer = (char*) malloc(chunk*sizeof(char));
 	ssize_t result;
 	
+	// Read data from input and write to output until EOF
 	while((result = read(fd_from, buffer, chunk)) != 0)
 	{
 		if(result == -1)
@@ -151,12 +176,15 @@ void copy_rw(int fd_from, int fd_to)
 			exit(1);
 		}
 	}
+
+	free(buffer);
 	
 	return;
 }
 
 void copy_mm(int fd_from, int fd_to)
 {
+	// Get size of input file
 	struct stat buf;
 	if(fstat(fd_from, &buf) == -1)
 	{
@@ -164,6 +192,7 @@ void copy_mm(int fd_from, int fd_to)
 		exit(1);
 	}
 
+	// Pointers to memory maps
 	char* buffer_in;
 	char* buffer_out;
 
@@ -173,6 +202,8 @@ void copy_mm(int fd_from, int fd_to)
 		perror("mmap in error\n");
 		exit(1);
 	}
+	
+	// Changing output file size to input file size before mapping
 	if(ftruncate(fd_to, buf.st_size))
 	{
 		perror("ftruncate error\n");
@@ -185,6 +216,7 @@ void copy_mm(int fd_from, int fd_to)
 		exit(1);
 	}
 
+	// Copy
 	buffer_out = memcpy(buffer_out, buffer_in, buf.st_size);
 	if(buffer_out == (void*) -1)
 	{
@@ -192,6 +224,7 @@ void copy_mm(int fd_from, int fd_to)
 		exit(1);
 	}
 
+	// Unmapping (unnecessary, but just in case)
 	if(munmap(buffer_in, buf.st_size) == -1)
 	{
 		perror("munmap in error\n");
@@ -202,4 +235,6 @@ void copy_mm(int fd_from, int fd_to)
 		perror("munmap out error\n");
 		exit(1);
 	}
+
+	return;
 }
