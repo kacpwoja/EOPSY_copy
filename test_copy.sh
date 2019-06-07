d6f3580336f3285a60d16038544e20541736f2b7
Kacper Piotr Wojakowski <kwojakow@uxn14.ia.pw.edu.pl>
2019-06-07 14:59:10 +0200

    test script

---
+++ test_copy.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+#Script for testing the copy program
+
+echo "Testing the copy script"
+
+echo "---------------------------"
+echo "Very small file: (56B)"
+t_start=`date +%N`
+./copy 'test/file_vsmall' 'test/file_copy_rw'
+t_end=`date +%N`
+runtime_rw=$(($t_end-$t_start))
+t_start=`date +%N`
+./copy '-m' 'test/file_vsmall' 'test/file_copy_mm'
+t_end=`date +%N`
+runtime_mm=$(($t_end-$t_start))
+echo "Runtime:"
+echo "RW:" $runtime_rw "ns"
+echo "MM:" $runtime_mm "ns"
+echo "Hash sums:"
+sha1sum 'test/file_vsmall'
+sha1sum 'test/file_copy_rw'
+sha1sum 'test/file_copy_mm'
+rm 'test/file_copy_rw' 'test/file_copy_mm'
+
+echo "---------------------------"
+echo "Small file: (1.4kB)"
+t_start=`date +%N`
+./copy 'test/file_small' 'test/file_copy_rw'
+t_end=`date +%N`
+runtime_rw=$(($t_end-$t_start))
+t_start=`date +%N`
+./copy '-m' 'test/file_small' 'test/file_copy_mm'
+t_end=`date +%N`
+runtime_mm=$(($t_end-$t_start))
+echo "Runtime:"
+echo "RW:" $runtime_rw "ns"
+echo "MM:" $runtime_mm "ns"
+echo "Hash sums:"
+sha1sum 'test/file_small'
+sha1sum 'test/file_copy_rw'
+sha1sum 'test/file_copy_mm'
+rm 'test/file_copy_rw' 'test/file_copy_mm'
+
+echo "---------------------------"
+echo "Medium file: (11.2kB)"
+t_start=`date +%N`
+./copy 'test/file_medium' 'test/file_copy_rw'
+t_end=`date +%N`
+runtime_rw=$(($t_end-$t_start))
+t_start=`date +%N`
+./copy '-m' 'test/file_medium' 'test/file_copy_mm'
+t_end=`date +%N`
+runtime_mm=$(($t_end-$t_start))
+echo "Runtime:"
+echo "RW:" $runtime_rw "ns"
+echo "MM:" $runtime_mm "ns"
+echo "Hash sums:"
+sha1sum 'test/file_medium'
+sha1sum 'test/file_copy_rw'
+sha1sum 'test/file_copy_mm'
+rm 'test/file_copy_rw' 'test/file_copy_mm'
+
+echo "---------------------------"
+echo "Big file: (112kB)"
+t_start=`date +%N`
+./copy 'test/file_big' 'test/file_copy_rw'
+t_end=`date +%N`
+runtime_rw=$(($t_end-$t_start))
+t_start=`date +%N`
+./copy '-m' 'test/file_big' 'test/file_copy_mm'
+t_end=`date +%N`
+runtime_mm=$(($t_end-$t_start))
+echo "Runtime:"
+echo "RW:" $runtime_rw "ns"
+echo "MM:" $runtime_mm "ns"
+echo "Hash sums:"
+sha1sum 'test/file_big'
+sha1sum 'test/file_copy_rw'
+sha1sum 'test/file_copy_mm'
+rm 'test/file_copy_rw' 'test/file_copy_mm'
+
+echo "---------------------------"
+echo "Very big file: (2.9MB)"
+t_start=`date +%N`
+./copy 'test/file_vbig' 'test/file_copy_rw'
+t_end=`date +%N`
+runtime_rw=$(($t_end-$t_start))
+t_start=`date +%N`
+./copy '-m' 'test/file_vbig' 'test/file_copy_mm'
+t_end=`date +%N`
+runtime_mm=$(($t_end-$t_start))
+echo "Runtime:"
+echo "RW:" $runtime_rw "ns"
+echo "MM:" $runtime_mm "ns"
+echo "Hash sums:"
+sha1sum 'test/file_vbig'
+sha1sum 'test/file_copy_rw'
+sha1sum 'test/file_copy_mm'
+rm 'test/file_copy_rw' 'test/file_copy_mm'
